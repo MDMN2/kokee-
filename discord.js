ee8fc2528a8a80867ae0923b324f24482328301e
MDMN <48229254+MDMN2@users.noreply.github.com>
2019-04-03 08:27:28 +0700

    Add files via upload

---
+++ discord.js
@@ -0,0 +1,41 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const prefix = '.'
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`Five Stars`,"http://twitch.tv/S-F")
+  console.log('')
+  console.log('')
+  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
+  console.log(`[Start] ${new Date()}`);
+  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════════════════════════════]╗');
+  console.log(`Logged in as * [ " ${client.user.username} " ]`);
+  console.log('')
+  console.log('Informations :')
+  console.log('')
+  console.log(`servers! [ " ${client.guilds.size} " ]`);
+  console.log(`Users! [ " ${client.users.size} " ]`);
+  console.log(`channels! [ " ${client.channels.size} " ]`);
+  console.log('╚[════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════]╗')
+  console.log(' Bot Is Online')
+  console.log('╚[════════════]╝')
+  console.log('')
+  console.log('')
+});
+
+client.login(NTYwOTQ0ODk1MjI2MDE5ODYx.XKQKqg.Z4xQKgU_RXM7-fulK_jVEhIKw_w);
+
+
+
+
+
+client.on('message', msg => {
+    if (msg.content === 'هلا') {
+       msg.reply('هلابيك:heart: ');
+      }
+    });
